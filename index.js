64f84455f0e12346812c231f83881a1be392eaa4
paulusbakker <paulusbakker@gmail.com>
2021-07-13 11:37:31 +0200

    version2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const {question} = require("readline-sync")
 const {displayWordSoFar, isGameWon, wrongGuesses} = require("./gamelogic")
 let letter = ""
+
 function game(word, guesses) {
     console.log(displayWordSoFar(word, guesses))
-    console.log("Dit heb je tot nu toe geraden: "+guesses)
-    console.log("Aantal foute letters "+wrongGuesses(word, guesses))
+    console.log("Dit heb je tot nu toe geraden: " + guesses)
+    console.log("Aantal foute letters " + wrongGuesses(word, guesses))
 
     while (true) {
         letter = question("Raad een letter: ")
@@ -20,10 +21,10 @@ function game(word, guesses) {
 
 
     if (isGameWon(word, guesses)) {
-        console.log("Super, gefeliciteerd")
+        console.log("Super, gefeliciteerd!")
         return
     }
-    if (wrongGuesses(word, guesses)===7) {
+    if (wrongGuesses(word, guesses) === 7) {
         console.log("Jammer, je hebt er 7 fout!")
         return
     }
